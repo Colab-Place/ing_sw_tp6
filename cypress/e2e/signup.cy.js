a9c2329044cf976040f620f617f3ee702a83e669
unknown <micaelaestrellamorales@gmail.com>
2023-08-22 20:32:03 -0300

    Create sign up tests

---
+++ cypress/e2e/signup.cy.js
@@ -0,0 +1,67 @@
+describe('Sign up tests', () => {
+  it('FAIL: Valid imput but existent email', () => {
+    cy.visit('/addUser');
+
+    cy.get('input[id="firstName"]').type('Peter');
+    cy.get('input[id="lastName"]').type('Parker');
+    cy.get('input[id="email"]').type('test@gmail.com');
+    cy.get('input[type="password"]').type('whatever');
+    cy.get('button[type="submit"]').click();
+
+    cy.get('#error')
+      .should('be.visible')
+      .should('contain.text', 'Email address is already in use');
+  });
+
+  it('FAIL: Uncomplete form', () => {
+    cy.visit('/addUser');
+
+    cy.get('input[id="email"]').type('test@gmail.com');
+    cy.get('input[type="password"]').type('whatever');
+    cy.get('button[type="submit"]').click();
+
+    cy.get('#error')
+      .should('be.visible')
+      // Debe aparecer un mensaje que nos avise de que
+      // no se completaron los campos de firstName y lastName
+      .should('contain.text', 'firstName')
+      .should('contain.text', 'lastName');
+  });
+
+  it('FAIL: Invalid email', () => {
+    cy.visit('/addUser');
+
+    cy.get('input[id="firstName"]').type('Peter');
+    cy.get('input[id="lastName"]').type('Parker');
+    cy.get('input[id="email"]').type('testgmail.com');
+    cy.get('input[type="password"]').type('whatever');
+    cy.get('button[type="submit"]').click();
+
+    cy.get('#error')
+      .should('be.visible')
+      .should('contain.text', 'Email is invalid');
+  });
+
+  it('FAIL: Short password', () => {
+    cy.visit('/addUser');
+
+    cy.get('input[id="firstName"]').type('Peter');
+    cy.get('input[id="lastName"]').type('Parker');
+    cy.get('input[id="email"]').type('test@gmail.com');
+    cy.get('input[type="password"]').type('w');
+    cy.get('button[type="submit"]').click();
+
+    cy.get('#error')
+      .should('be.visible')
+  });
+
+  it('SUCCESS: Cancel button returns to homepage', () => {
+    cy.visit('/addUser');
+
+    cy.get('button[id="cancel"]')
+      .should('be.visible')
+      .click()
+
+    cy.location('pathname').should('eq', '/login')
+  });
+})
