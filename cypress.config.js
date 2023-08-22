27b98e38c892980e975c456d39e3e45ae2693303
unknown <micaelaestrellamorales@gmail.com>
2023-08-22 19:18:07 -0300

    Create cypress project

---
+++ cypress.config.js
@@ -0,0 +1,7 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://thinking-tester-contact-list.herokuapp.com/',
+  },
+})
---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,5 @@
+describe('template', () => {
+  it('visit webpage', () => {
+    cy.visit('/')
+  })
+})
